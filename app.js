98bdb86769db228566866ca30d9331274932b4b9
gczene <gabor@czene.co.uk>
2016-08-23 10:03:01 +0100

    default page

--- app.js
+++ app.js
@@ -3,28 +3,45 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   request = require('superagent');
 
+const host = 'https://www.britishgas.co.uk/';
+
 app.set('view engine', 'pug');
 
 app.use(express.static('public'));
 
-app.get('/:id', function (req, res, next) {
-  let url = `https://www.britishgas.co.uk/${req.params.id}`;
 
-  request.get(url)
-    .end((err) => {
-      if (err) {
-        next(err);
-      } else {
-        res.render('index', {
-          title: 'my title',
-          text: 'my text'
-        });
-      }
-    });
+app.get('/info/', function (req, res) {
+  res.render('default', {});
+});
+
+app.get('/info/*?', function (req, res, next) {
+  let path = req.params[0];
+
+  if (!path) {
+    next('no get params');
+  } else {
+    let url = `${host}${path}`;
+
+    request.get(url)
+      .end((err, r) => {
+        console.log(r);
+        if (err) {
+          next(err);
+        } else {
+          res.render('index', {
+            title: 'my title dsadsa',
+            text: 'my text',
+            backlink: host + path
+          });
+        }
+      });
+
+  }
 
 });
 
 app.use((err, req, res, next) => {
+  console.error(err.toString());
   res.send('error happened');
 });
 
